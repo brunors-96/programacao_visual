246601fdd04fd15678c860dab40eb466755492b6
brunors-96 <brunors-96@hotmail.com>
2017-09-06 00:08:23 -0300

    Capitulo 2. Terminei o 2.1, começar lendo 2.2

---
+++ WebApplication1/Contexts/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using System.Data.Entity;
+
+namespace WebApplication1.Contexts
+{
+    public class EFContext/*:DbContext*/ 
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+
+        public DbSet<Categorie> Categories { get; set; }
+        public DbSet<Builder> Builders { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Builder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Builder
+    {
+        public long BuilderId { get; set; }
+        public string Nome { get; set; }
+    }
+}
